e37c5f8230606e62eb9f3d303bb1b937c7149459
Thomas Martin <Tom@Toms-MacBook-Pro.local>
2014-02-03 18:50:56 +0000

    Working humand player vs. human player tictactoe game

---
+++ tictactoe.rb
@@ -0,0 +1,114 @@
+# class TicTacToe
+
+  require "color_text"
+
+  # def initialize
+    @board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
+    @turn = 0
+    @winning_positions = [[0, 4, 8], [3, 4, 5], [1, 4, 7], [2, 4, 6], [0, 1, 2], [0, 3, 6], [6, 7, 8], [2, 5, 8]]
+  # end
+
+  def menu
+    print `clear`
+    puts "***Ruby TicTacToe***"
+    print "Choose to (p)lay the game or (q)uit: "
+    gets.chomp.downcase
+  end
+
+  def boardNotFull?
+    @board.include?(" ")    
+  end
+
+  def startWith
+    @turn = rand(2)
+  end
+
+  def turn
+    @turn % 2 == 0 ? "X" : "O" 
+  end
+
+  def renderGrid
+    puts
+    puts "1 | 2 | 3"
+    puts "- + - + -"
+    puts "4 | 5 | 6"
+    puts "- + - + -"
+    puts "6 | 8 | 9"
+  end
+
+  def renderBoard
+    puts
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "- + - + -"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "- + - + -"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
+
+  def game
+
+
+    winning_position = false
+    while winning_position == false && boardNotFull?     
+      nextTurn
+      winning_position = hasWon?      
+    end
+    renderBoard
+    puts "Nobody won!" if !winning_position && boardNotFull? == false
+    gets
+    menu
+  end
+
+  def nextTurn
+    who = turn      
+    renderGrid
+    renderBoard                 
+    print who + "'s turn. " 
+
+    choice = ""
+    while choice == "" do
+      choice = gets.chomp.to_i
+      if @board[choice - 1] != " "
+        puts "There is already a #{@board[choice - 1]} there!, choose another: "
+        choice = ""
+      end
+    end
+
+    @board[choice - 1] = turn.downcase     
+    @turn += 1                  
+  end
+
+  def hasWon?
+    # if @turn < 3
+    #   return false
+    # else
+    player = nil
+    # while @turn < 2
+    @winning_positions.each { |a|
+      player = @board[a[0]]
+      if player != " " && (@board[a[0]] == @board[a[1]]) && (@board[a[1]] == @board[a[2]])
+        puts "Congratulations " + player + " you have won!"
+        return player
+        gets
+        menu
+      end
+    }
+    return false
+  # end
+  end
+
+  game
+
+response = menu
+ 
+while response != 'q'
+  case response
+  when 'p'
+    game 
+  end
+ 
+  response = menu
+end
+
+# end
+
